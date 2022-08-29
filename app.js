08af3fbf3ef9f3866fc55170a93329e25638418d
CitySim <sven31894@gmail.com>
2022-08-29 19:29:37 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,293 @@
+import { DataStore } from "./DataStore.js";
+
+class App {
+    constructor() {
+        this.store = new DataStore()
+        this.store.load().then(() => {
+            this.render();
+        });
+
+        this.addToQueue = this.addToQueue.bind(this);
+    }
+
+    addToQueue(workshop, itemId) {
+        const item = this.store.MJICraftworksObject.find(item => item.ID === itemId);
+        if (item == null) return;
+        this.store.config.workshops[workshop].queue.push(item)
+        
+        this.render();
+    }
+
+    render() {
+        // render Workshops
+        for (let i = 0; i < 3; i++) this.renderWorkshop(i)
+        // render add item
+        this.renderAddItem();
+        // render Config
+        this.renderConfig();
+    }
+
+    renderWorkshop(workshop) {
+        const elWorkShop = document.getElementById(`workshop-${workshop}`)
+        
+        function renderTable() {
+            let offset = 10;
+            let out = "";
+
+            const itemByHour = {};
+            let itemByHourOffset = 0;
+            this.store.config.workshops[workshop].queue.forEach(item => {
+                itemByHour[itemByHourOffset] = item;
+                itemByHourOffset += item.CraftingTime;
+            })
+
+            let previousItem;
+            for (let hour = 0; hour < 24; hour++) {
+                const timeDisplay = (offset + hour) >= 24 ? `${offset + hour - 24}:00` : `${offset + hour}:00`
+                const item = itemByHour[hour];
+
+                let value = 0;
+                let efficiencyBonus = false;
+                if (item != null) {
+                    [ value, efficiencyBonus ] = this.calculateItemValue({ workshop, item, previousItem })
+                }
+
+                out += `
+                    <tr>
+                        <td>${timeDisplay}</td>
+                        ${item != null? `
+                            <td rowSPan="${item.CraftingTime}" style="border: 1px solid black;">
+                                <img src="https://xivapi.com/${item.Item.IconHD}" style="height: 1em;"/>
+                                ${item.Item.Name}<br/>
+                                ${item.Theme0?.Name ?? ""}${item.Theme1 != null ? " / " : ""}${item.Theme1?.Name ?? ""}<br/>
+                                ${efficiencyBonus ? "Efficiency Bonus!" : "No Bonus!"}<br/>
+                                Value ${value}
+                            </td>
+                        ` : ""}
+                    </tr>
+                `
+
+                if (item != null) previousItem = item;
+            }
+
+            return `
+                <table>
+                    <tbody>
+                        ${out}
+                    </tbody>
+                </table>
+            `;
+        }
+
+        elWorkShop.onclick = () => {
+            this.addItemState = { workshop };
+            this.render();
+        }
+
+        elWorkShop.innerHTML = `
+            Workshop ${workshop + 1}
+            Rank
+            <select>
+                <option value="0" ${this.store.config.workshops[0].rank === 0 ? "selected" : ""}></option>
+                <option value="1" ${this.store.config.workshops[0].rank === 1 ? "selected" : ""}>1</option>
+                <option value="2" ${this.store.config.workshops[0].rank === 2 ? "selected" : ""}>2</option>
+                <option value="3" ${this.store.config.workshops[0].rank === 3 ? "selected" : ""}>3</option>
+            </select>
+
+            ${renderTable.bind(this)()}
+        `;
+    }
+
+    renderAddItem() {
+        const elAddItem = document.getElementById("add-item");
+        if (this.addItemState == null) {
+            elAddItem.innerHTML = "";
+            return;
+        }
+
+        const workshop = this.addItemState.workshop;
+        const queue = this.store.config.workshops[workshop].queue;
+        const previousItem = queue[queue.length - 1]
+
+        const [ goodItems ] = this.findItems(workshop);
+        goodItems.sort((a, b) => {
+            const [ valueA ] = this.calculateItemValue({ workshop, item: a, previousItem });
+            const [ valueB ] = this.calculateItemValue({ workshop, item: b, previousItem });
+
+            return (valueB / b.CraftingTime) - (valueA / a.CraftingTime);
+        })
+
+        const popName = {
+            1: "Very High",
+            2: "High",
+            3: "Average",
+            4: "Low",
+        }
+
+        elAddItem.innerHTML = `
+            <table border="1">
+                <thead>
+                    <tr>
+                    <th>Item</th>
+                    <th>Theme</th>
+                    <th>Theme</th>
+                    <th>Efficiency</th>
+                    <th>Time</th>
+                    <th>Popularity</th>
+                    <th>Value</th>
+                    <th>Value / Hour (v)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${goodItems.map(item => {
+                        const [ value, efficiencyBonus ] = this.calculateItemValue({ workshop, item, previousItem });
+                        const pop = this.store.MJICraftWorksPopularity[this.store.config.popularity][item.ID];
+
+                        return `
+                            <tr onClick="window.app.addToQueue(${workshop}, ${item.ID})">
+                                <td>
+                                    <img src="https://xivapi.com/${item.Item.IconHD}" style="height: 1em;"/>
+                                    ${item.Item.Name}
+                                </td>
+                                <td>
+                                    ${item.Theme0?.Name ?? ""}
+                                </td>
+                                <td>
+                                    ${item.Theme1?.Name ?? ""}
+                                </td>
+                                <td>
+                                    ${efficiencyBonus ? "yes" : "no"}
+                                </td>
+                                <td>
+                                    ${item.CraftingTime}h
+                                </td>
+                                <td>
+                                    ${popName[pop] ?? pop}
+                                </td>
+                                <td>
+                                    ${value}
+                                </td>
+                                <td>
+                                    ${(value / item.CraftingTime).toFixed(2)}
+                                </td>
+                            </tr>
+                        `
+                    }).join("")}
+                </tbody>
+            </table>
+        `;
+    }
+    
+    renderConfig() {
+        const elConfig = document.getElementById("config");
+
+        function renderSelect(index) {
+            const select = document.createElement("select")
+            select.onchange = (e) => {
+                this.store.config.veryHighItems[index] = parseInt(e.target.value);
+                this.render()
+            }
+
+            select.innerHTML = `
+                <option value="0"></option>
+                ${this.store.MJICraftworksObject.filter(i => i.Item != null).map(item => {
+                    return `
+                        <option value="${item.ID}" ${this.store.config.veryHighItems[index] === item.ID ? "selected" : ""}>${item.Item.Name}</option>
+                    `
+                }).join("")}
+            `
+
+            return select;
+        }
+
+        elConfig.innerHTML = `
+            Very High Items:<br/>
+        `;
+
+        elConfig.append(renderSelect.bind(this)(0));
+        elConfig.append(renderSelect.bind(this)(1));
+        elConfig.append(renderSelect.bind(this)(2));
+        elConfig.append(renderSelect.bind(this)(3));
+        elConfig.append(renderSelect.bind(this)(4));
+
+        const matrixList = Object.entries(this.store.MJICraftWorksPopularity).filter(([ index, popMatrix ]) => {
+            return this.store.config.veryHighItems.every(item => popMatrix[item] === 1)
+        })
+        console.log(matrixList)
+        elConfig.append(`matches ${matrixList.length}`);
+
+        if (matrixList.length === 1) {
+            const newPop = parseInt(matrixList[0]);
+            if (this.store.config.popularity !== newPop) {
+                this.store.config.popularity = parseInt(matrixList[0]);
+                this.render();
+            }
+        }
+    }
+
+    findItems(workshop) {
+        const activeThemes = [];
+        let freeHours = 24;
+        
+        const workshopData = this.store.config.workshops[workshop];
+        if (workshopData.queue.length > 0) {
+            const lastItem = workshopData.queue[workshopData.queue.length - 1];
+            if (lastItem.Theme0TargetID !== 0) activeThemes.push(lastItem.Theme0TargetID);
+            if (lastItem.Theme1TargetID !== 0) activeThemes.push(lastItem.Theme1TargetID);
+
+            freeHours = 24 - workshopData.queue.reduce((sum, item) => sum + item.CraftingTime, 0);
+        }
+
+        const goodItems = [];
+        const badItems = [];
+
+        this.store.MJICraftworksObject.forEach(item => {
+            if (item.Item == null) return;
+
+            // console.log(item.Item.Name, item.Theme0TargetID, item.Theme1TargetID, item.CraftingTime);
+
+            if (
+                activeThemes.length > 0
+                && (item.Theme0TargetID === 0 || activeThemes.includes(item.Theme0TargetID) === false)
+                && (item.Theme1TargetID === 0 || activeThemes.includes(item.Theme1TargetID) === false)
+            ) {
+                badItems.push(item);
+                return
+            }
+            if (freeHours < item.CraftingTime) {
+                badItems.push(item);
+                return
+            }
+            
+            goodItems.push(item);
+        })
+
+        return [ goodItems, badItems ]
+    }
+
+    calculateItemValue({ workshop, item, previousItem }) {
+        // efficiency bonus
+        const hasEfficiencyBonus = item.ID !== previousItem?.ID && (
+            item.Theme0TargetID === previousItem?.Theme0TargetID || item.Theme0TargetID === previousItem?.Theme1TargetID
+            || item.Theme1TargetID === previousItem?.Theme0TargetID || item.Theme1TargetID === previousItem?.Theme1TargetID
+        )
+        const efficiencyBonus = hasEfficiencyBonus === true ? 2 : 1;
+        // pop
+        const pop = this.store.MJICraftWorksPopularity[this.store.config.popularity][item.ID];
+        const popularityMod = this.store.MJICraftWorksPopularityType[pop] / 100;
+        // supply
+        const supplyMod = this.store.MJICraftWorksSupplyDefine[2].factor / 100;
+        // workshop rank
+        const workshopRank = this.store.config.workshops[workshop].rank;
+        const workshopRankMod = this.store.MJICraftWorksRankRatio[workshopRank] / 100;
+        // groove
+        const groove = 0;
+        
+        return [
+            efficiencyBonus * Math.floor(popularityMod * supplyMod * Math.floor(item.Value * workshopRankMod * (1 + groove / 100))),
+            hasEfficiencyBonus,
+        ];
+    }
+}
+
+window.app = new App();
