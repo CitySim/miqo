8827072cd0191f01f583ea63c40a1539c3db3637
CitySim <sven31894@gmail.com>
2022-08-29 20:05:03 +0200

    many minor fixes

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ class App {
             this.render();
         });
 
+        this.clearQueue = this.clearQueue.bind(this);
+        this.removeFromQueue = this.removeFromQueue.bind(this);
         this.addToQueue = this.addToQueue.bind(this);
+    }
+
+    clearQueue(workshop) {
+        this.store.config.workshops[workshop].queue = [];
+
+        this.render();
+    }
+
+    removeFromQueue(workshop) {
+        const queue = this.store.config.workshops[workshop].queue
+        queue.splice(queue.length - 1, 1)
+        
+        this.render();
     }
 
     addToQueue(workshop, itemId) {
@@ -29,14 +44,16 @@ class App {
 
     renderWorkshop(workshop) {
         const elWorkShop = document.getElementById(`workshop-${workshop}`)
-        
+        let totalValue = 0;
+
         function renderTable() {
             let offset = 10;
             let out = "";
 
+            const queue = this.store.config.workshops[workshop].queue;
             const itemByHour = {};
             let itemByHourOffset = 0;
-            this.store.config.workshops[workshop].queue.forEach(item => {
+            queue.forEach(item => {
                 itemByHour[itemByHourOffset] = item;
                 itemByHourOffset += item.CraftingTime;
             })
@@ -50,7 +67,10 @@ class App {
                 let efficiencyBonus = false;
                 if (item != null) {
                     [ value, efficiencyBonus ] = this.calculateItemValue({ workshop, item, previousItem })
+                    totalValue += value;
                 }
+
+                const isLast = queue[queue.length - 1] === item
 
                 out += `
                     <tr>
@@ -58,7 +78,11 @@ class App {
                         ${item != null? `
                             <td rowSPan="${item.CraftingTime}" style="border: 1px solid black;">
                                 <img src="https://xivapi.com/${item.Item.IconHD}" style="height: 1em;"/>
-                                ${item.Item.Name}<br/>
+                                ${item.Item.Name}
+                                ${isLast ? `
+                                    <button onClick="window.app.removeFromQueue(${workshop})">x</button>
+                                ` : ""}
+                                <br/>
                                 ${item.Theme0?.Name ?? ""}${item.Theme1 != null ? " / " : ""}${item.Theme1?.Name ?? ""}<br/>
                                 ${efficiencyBonus ? "Efficiency Bonus!" : "No Bonus!"}<br/>
                                 Value ${value}
@@ -76,10 +100,16 @@ class App {
                         ${out}
                     </tbody>
                 </table>
+
+                Total Value: ${totalValue}
+                <button onClick="window.app.clearQueue(${workshop})">clear</button>
             `;
         }
 
         elWorkShop.onclick = () => {
+            document.querySelectorAll(".workshop").forEach(e => e.classList.remove("active"))
+            elWorkShop.classList.add("active")
+
             this.addItemState = { workshop };
             this.render();
         }
@@ -134,7 +164,8 @@ class App {
                     <th>Efficiency</th>
                     <th>Time</th>
                     <th>Popularity</th>
-                    <th>Value</th>
+                    <th>Base Value</th>
+                    <th>Final Value</th>
                     <th>Value / Hour (v)</th>
                     </tr>
                 </thead>
@@ -163,6 +194,9 @@ class App {
                                 </td>
                                 <td>
                                     ${popName[pop] ?? pop}
+                                </td>
+                                <td>
+                                    ${item.Value}
                                 </td>
                                 <td>
                                     ${value}
@@ -202,6 +236,7 @@ class App {
 
         elConfig.innerHTML = `
             Very High Items:<br/>
+            Select several of your "Very High" popularity item until there is only one match left.
         `;
 
         elConfig.append(renderSelect.bind(this)(0));
@@ -211,10 +246,25 @@ class App {
         elConfig.append(renderSelect.bind(this)(4));
 
         const matrixList = Object.entries(this.store.MJICraftWorksPopularity).filter(([ index, popMatrix ]) => {
-            return this.store.config.veryHighItems.every(item => popMatrix[item] === 1)
+            return this.store.config.veryHighItems.every(item => item === 0 || item == null || popMatrix[item] === 1)
         })
         console.log(matrixList)
         elConfig.append(`matches ${matrixList.length}`);
+
+        const extraDiv = document.createElement("div")
+        extraDiv.innerHTML = `
+            <hr>
+            <a href="https://github.com/CitySim/miqo" target="_blank">GitHub</a><br/>
+
+            <ul>
+                <li>this entire thing is a bit of a mess right now :)</li>
+                <li>assumes you have a rank 10 island, and rank 3 workshops</li>
+                <li>supply is entirely ignored currently</li>
+                <li>groove is also missing at the moment</li>
+                <li>thanks to this <a href="https://docs.google.com/spreadsheets/d/1e5dyaHSt5lj25l3nFWO5QcPmAJ2aAoPxCWj-iZnKxRk/edit#gid=1283864903" target="_blank">sheet</a> for the value formula</a>
+            </ul>
+        `
+        elConfig.append(extraDiv)
 
         if (matrixList.length === 1) {
             const newPop = parseInt(matrixList[0]);
